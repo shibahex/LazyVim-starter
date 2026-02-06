9d4bc3bb191fbdf70a00bbcdaa0a3f73dff8e6d2
shibahex <badamsva@gmail.com>
2026-02-06 15:41:16 -0500

    chore: fix keymapping and errors

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,6 @@
 return {
   "mbbill/undotree",
-  vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Open [U]ndo tree" }),
+  vim.keymap.set("n", "<leader>zu", vim.cmd.UndotreeToggle, { desc = "Open [U]ndo tree" }),
   -- Enable persistent undo with Lua config
   config = function()
     vim.opt.undofile = true
