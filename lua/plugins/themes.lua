c9a506abe5ebb4b5e0df574f04bde1a7a613af0b
shibahex <badamsva@gmail.com>
2026-02-05 23:09:47 -0500

    add: more custom themes!

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,20 @@
+return {
+  { "rebelot/kanagawa.nvim" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+  { "Mofiqul/vscode.nvim" },
+  { "marko-cerovac/material.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "sainnhe/sonokai" },
+  { "tiagovla/tokyodark.nvim" },
+  { "Yazeed1s/oh-lucy.nvim" },
+  { "kvrohit/mellow.nvim" },
+  { "armannikoyan/rusty" },
+  { "Mofiqul/adwaita.nvim" },
+  { "sainnhe/edge" },
+}
