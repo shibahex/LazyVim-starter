bfbafe900eb5d80d949c9c202e624e19c994341a
shibahex <badamsva@gmail.com>
2026-02-07 20:43:05 -0500

    feat: add keymaps for virtual_lines and a way to copy errors, all under the debug tab.

--- lua/plugins/lazyvim_tweaks.lua
+++ lua/plugins/lazyvim_tweaks.lua
@@ -7,8 +7,39 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       diagnostics = {
-        virtual_lines = true,
-        virtual_text = false,
+        virtual_lines = false,
+        virtual_text = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>dd",
+        function()
+          local current_lines = vim.diagnostic.config().virtual_lines
+
+          if current_lines then
+            -- Switch from virtual_lines to virtual_text
+            vim.diagnostic.config({
+              virtual_lines = false,
+              virtual_text = true,
+            })
+            vim.notify("Switched to virtual text", vim.log.levels.INFO)
+          else
+            -- Switch from virtual_text to virtual_lines
+            vim.diagnostic.config({
+              virtual_lines = true,
+              virtual_text = false,
+            })
+            vim.notify("Switched to virtual lines", vim.log.levels.INFO)
+          end
+        end,
+        desc = "Toggle diagnostic display mode",
+      },
+      {
+        "<leader>de",
+        vim.diagnostic.setloclist,
+        desc = "Open diagnostic [E]rrors list",
+        vim.notify("Showing Errors", vim.log.levels.INFO),
       },
     },
   },
